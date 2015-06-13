045890082378405e2a3ec03a4a674dbfe7e1b8d6
Keenan Sturtevant <keenansturtevant@gmail.com>
2015-06-13 16:16:39 +0800

    data searching

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,55 @@
 class StaticPagesController < ApplicationController
   def home
+  @unfinishedsosss = [] 
+    SalesOrder.all.each do |order|
+      if order.due_date == "" 
+        @unfinishedsosss << order
+      elsif order.bill_to == "" 
+        @unfinishedsosss << order
+      elsif order.requestor_name == "" 
+        @unfinishedsosss << order
+      elsif order.title == "" 
+        @unfinishedsosss << order
+      elsif order.address == "" 
+        @unfinishedsosss << order
+      elsif order.email == "" 
+        @unfinishedsosss << order
+      elsif order.origin_port == "" 
+        @unfinishedsosss << order
+      elsif order.mode == "" 
+        @unfinishedsosss << order
+      elsif order.service_level == "" 
+        @unfinishedsosss << order
+      elsif order.shipper_name == "" 
+        @unfinishedsosss << order
+      elsif order.shipper_address == "" 
+        @unfinishedsosss << order
+      elsif order.importer_of_record == "" 
+        @unfinishedsosss << order
+      elsif order.customs_origin_broker == "" 
+        @unfinishedsosss << order
+      elsif order.customs_dest_broker == "" 
+        @unfinishedsosss << order
+      elsif order.origin_airport == "" 
+        @unfinishedsosss << order
+      elsif order.actual_weight == "" 
+        @unfinishedsosss << order
+      elsif order.uom == "" 
+        @unfinishedsosss << order
+      elsif order.pcs == "" 
+        @unfinishedsosss << order
+      elsif order.dimensions_length == "" 
+        @unfinishedsosss << order
+      elsif order.dimensions_width == "" 
+        @unfinishedsosss << order
+      elsif order.dimensions_height == "" 
+        @unfinishedsosss << order
+      elsif order.chargeable_weight == nil
+        @unfinishedsosss << order
+      else
+        # @unfinishedsosss = "all soss's are complete"
+      end
+    end
   end
 
   def help
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+(5).times do 
+SalesOrder.create(
+due_date: Faker::Date.forward(23),
+bill_to: Faker::Address.street_name,
+requestor_name: Faker::Name.name,
+title: Faker::Company.name,
+address: Faker::Address.street_address,
+email: Faker::Internet.email,
+origin_port: Faker::Address.country_code,
+mode: "ground",
+service_level: "high",
+shipper_name: "DWMorgan",
+shipper_address: Faker::Address.street_address,
+importer_of_record: "USA",
+customs_origin_broker: Faker::Address.country_code,
+customs_dest_broker: Faker::Address.country_code,
+origin_airport: Faker::Address.country_code,
+actual_weight: Faker::Number.number(3),
+uom: Faker::Number.number(11),
+pcs: Faker::Number.number(2),
+dimensions_length: Faker::Number.number(1),
+dimensions_width: Faker::Number.number(1),
+dimensions_height: Faker::Number.number(1),
+chargeable_weight: Faker::Number.number(3) 
+  )
+end
